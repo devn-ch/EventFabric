1822b9c5003069353c275cbd9caf469a0bef426e
devn-ch <233639015+devn-ch@users.noreply.github.com>
2026-06-19 14:22:04 +0200

    doc-24: starte mit übersetzung auf deutsch

---
+++ docs/astro.config.mjs
@@ -0,0 +1,69 @@
+// @ts-check
+import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config';
+import mermaid from 'astro-mermaid';
+import remarkGfm from 'remark-gfm';
+import { starlightBasePath } from "starlight-base-path";
+
+// https://astro.build/config
+export default defineConfig({
+	base: "/EventFabric",
+	integrations: [
+		mermaid({
+		theme: 'forest',
+		autoTheme: true
+		}),
+		starlight({
+			title: 'EventFabric CQRS Framework',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/devn-ch/EventFabric' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						{ label: 'What is EventFabric?', slug: 'guides/what-is-eventfabric' },
+						{ label: 'Philosophy', slug: 'guides/philosophy' },
+						{ label: 'Get Started', slug: 'guides/get-started' },
+						{ label: 'Architecture', slug: 'guides/architecture' },
+						{ label: 'The Era of AI', slug: 'guides/era-of-ai' },
+						{ label: 'In Depth Example', slug: 'guides/in-depth-example' },
+						{ label: 'Glossary', slug: 'guides/glossary' },
+					],
+				},
+				{
+					label: 'Core',
+					items: [{ autogenerate: { directory: 'core' } }],
+				},
+				{
+					label: 'Hono',
+					items: [{ autogenerate: { directory: 'hono' } }],
+				},
+				{
+					label: 'EventSourcingDB',
+					items: [{ autogenerate: { directory: 'eventsourcingdb' } }],
+				},
+				{
+					label: 'MongoDB',
+					items: [{ autogenerate: { directory: 'mongodb' } }],
+				},
+				{
+					label: 'Utils',
+					items: [{ autogenerate: { directory: 'utils' } }],
+				},
+			],
+			locales: {
+				root: {
+					label: 'English',
+					lang: 'en',
+				},
+				de: {
+					label: 'Deutsch',
+					lang: 'de',
+				},
+			},
+			plugins: [starlightBasePath()],
+		}),
+	],
+	markdown: {
+    	remarkPlugins: [remarkGfm], // Fügt Unterstützung für Tabellen hinzu
+  	},
+});
